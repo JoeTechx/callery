1f6cbd5491fecb84cb0b84b3ce5d4108bde38454
JOEX <oyinjoe23@gmail.com>
2023-09-19 04:22:02 +0100

    feat: create car_db

---
+++ src/car_Db.js
@@ -0,0 +1,131 @@
+const Toyota1 = require("./Assets/supra1.jpg");
+const Toyota2 = require("./Assets/supra2.jpeg");
+const Toyota3 = require("./Assets/supra3.jpg");
+const Toyota4 = require("./Assets/Toyota-GR-Supra.jpg");
+const Toyota5 = require("./Assets/supra5.jpg");
+
+const NissanGtr1 = require("./Assets/nissangtr1.jpg");
+const NissanGtr2 = require("./Assets/nissangtr2.jpg");
+
+const Tesla = require("./Assets/tesla.jpg");
+
+const Ferrari = require("./Assets/ferrari.jpg");
+const Ferrari1 = require("./Assets/ferrari.jpeg");
+const Ferrari2 = require("./Assets/ferrari1.jpg");
+const Ferrari_Monze_SP2 = require("./Assets/Ferrari_Monze_SP2.jpg");
+
+const Arudi_Abt = require("./Assets/Arudi_Abt.jpg");
+const arudi1 = require("./Assets/arudi1.jpg");
+
+const Black_BMW_car = require("./Assets/Black_BMW_car.jpg");
+const bmw_f82 = require("./Assets/bmw_f82.jpg");
+
+const Black_lamboorghini_aventador = require("./Assets/black_lamborghini_aventador_4k_hd.jpg");
+
+const HondaSport = require("./Assets/Honda.jpg");
+
+const Acura_NSX = require("./Assets/Acura_NSX.jpg");
+const Acura1 = require("./Assets/Acura1.jpg");
+
+export default images = [
+  {
+    id: 1,
+    image: Toyota1,
+    Tag: "Toyota Supra",
+  },
+  {
+    id: 2,
+    image: Toyota2,
+    Tag: "Toyota Supra",
+  },
+  {
+    id: 3,
+    image: Toyota3,
+    Tag: "Toyota Supra",
+  },
+  {
+    id: 4,
+    image: Toyota4,
+    Tag: "Toyota Supra",
+  },
+  {
+    id: 5,
+    image: Toyota5,
+    Tag: "Toyota Supra",
+  },
+  {
+    id: 6,
+    image: NissanGtr1,
+    Tag: "Nissan Gtr skyline",
+  },
+  {
+    id: 7,
+    image: NissanGtr2,
+    Tag: "Nissan Gtr skyline",
+  },
+  {
+    id: 8,
+    image: Tesla,
+    Tag: "Tesla",
+  },
+  {
+    id: 9,
+    image: Ferrari,
+    Tag: "Ferrari",
+  },
+  {
+    id: 10,
+    image: Ferrari1,
+    Tag: "Ferrari",
+  },
+  {
+    id: 11,
+    image: Ferrari2,
+    Tag: "Ferrari",
+  },
+  {
+    id: 12,
+    image: Ferrari_Monze_SP2,
+    Tag: "Ferrari_Monze_SP2",
+  },
+  {
+    id: 13,
+    image: Arudi_Abt,
+    Tag: "Arudi_Abt",
+  },
+  {
+    id: 14,
+    image: arudi1,
+    Tag: "Arudi",
+  },
+  {
+    id: 15,
+    image: Black_BMW_car,
+    Tag: "Black_BMW_car",
+  },
+  {
+    id: 16,
+    image: bmw_f82,
+    Tag: "bmw_f82",
+  },
+  {
+    id: 17,
+    image: Black_lamboorghini_aventador,
+    Tag: "Black_lamboorghini_aventador",
+  },
+  {
+    id: 18,
+    image: HondaSport,
+    Tag: "HondaSport",
+  },
+  {
+    id: 19,
+    image: Acura_NSX,
+    Tag: "Acura_NSX",
+  },
+  {
+    id: 20,
+    image: Acura1,
+    Tag: "Acura",
+  },
+];
