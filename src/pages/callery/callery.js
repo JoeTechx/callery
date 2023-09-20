be44b46c36c48bddcc4a56213b104a80ed7db4ae
JOEX <oyinjoe23@gmail.com>
2023-09-20 18:35:04 +0100

    feat: nav

--- src/pages/callery/callery.js
+++ src/pages/callery/callery.js
@@ -1,50 +1,12 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { auth } from "../../firebase/firebaseConfig";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import  LogoWhite from "../../Assets/logo white.png"
+import Nav from "../../Component/nav/nav";
+import "./callery.css"
 
 const Callery = () => {
-  const navigate = useNavigate();
-  const [authUser, setAuthUser] = useState(null);
-  const submit = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        alert("Signed Out");
-        navigate("/");
-      })
-      .catch((error) => {
-        // An error happened.
-      });
-  };
 
-  useEffect(() => {
-    const listen = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setAuthUser(user);
-      } else {
-        setAuthUser(null);
-      }
-    });
-  }, []);
 
   return (
     <>
-
-   < Link to="/carllery">
-    <img src={LogoWhite} alt=""/>
-   </Link>
-      {authUser ? (
-        <h2>Welcome {authUser.email.length > 6 ? (authUser.email.substr(0, 6)) + "..." : authUser.email}</h2>
-      ) : (
-        <Link to="/">login</Link>
-      )
-
-      }
-      <button className="logout" onClick={submit}>
-        Logout
-      </button>
+<Nav/>
     </>
   );
 };
